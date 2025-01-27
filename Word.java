c78a162330ed5ff3606c692f5b62328521465698
bHammons07 <brody.hammons@stu.daviess.kyschools.us>
2025-01-27 15:11:57 +0000

    ah

--- Word.java
+++ Word.java
@@ -3,44 +3,56 @@ import java.util.ArrayList;
 
 public class Word
 {
+	//Instance field(variable)
 	private String word;
-	private static String vowels = "AEIOUaeiou";   //static means only one
+
+	//Class field(variables)
+	private static final String VOWELS = "AEIOUaeiou";   //static means only one
 
 
 	public Word()
 	{
-
+		word = "";
 	}
 
 	public Word(String newWord)
 	{
-
+		setWord(word);
 	}
 
 	public void setWord(String newWord)
 	{
-
+		word = newWord;
 	}
 
 	public int getNumVowels()
 	{
-
+		int count = 0;
 
 		//Loop for every letter in "word"
-
+		for(int i = 0; i < word.length(); i++)
+		{
+			if(VOWELS.indexOf.word.substring(i,i+1))
+			{
+				count++;
+			}
+		}
 			//Use indexOf to see if the letter is in the string "vowels"
-
 
 		return count;
 	}
 
 	public int getLength()
 	{
+		int str = 0;
 
+		str = word.length();
+
+		return str;
 	}
 
 	public String toString()
 	{
-	   return word;
+	   return "" + word;
 	}
 }
